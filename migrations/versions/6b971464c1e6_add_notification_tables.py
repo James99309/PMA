57bf1428ea508b1f3aa0808369ae089b8bc9e15d
倪捷 <nijie@Jamess-MacBook-Pro.local>
2025-05-18 09:59:35 +0800

    feat: 添加通知系统相关表并实现订阅功能

---
+++ migrations/versions/6b971464c1e6_add_notification_tables.py
@@ -0,0 +1,24 @@
+"""add notification tables
+
+Revision ID: 6b971464c1e6
+Revises: 1c63c14bcc12
+Create Date: 2025-05-18 09:56:56.876739
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6b971464c1e6'
+down_revision = '1c63c14bcc12'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
