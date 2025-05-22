c408715c61d271083bfd05f0a79f08018ac52cc7
倪捷 <nijie@Jamess-MacBook-Pro.local>
2025-05-22 17:25:35 +0800

    add initial migration files

---
+++ migrations/versions/97cfe4b0eb27_initial_for_render.py
@@ -0,0 +1,25 @@
+
+"""initial for render
+
+Revision ID: 97cfe4b0eb27
+Revises: 805c8f258a33
+Create Date: 2025-05-22 16:59:18.074972
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '97cfe4b0eb27'
+down_revision = '805c8f258a33'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
