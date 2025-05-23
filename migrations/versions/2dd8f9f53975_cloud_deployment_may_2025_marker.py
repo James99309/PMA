12e408628a8e8aba172e93fd0fd35fe2740fa5ca
倪捷 <nijie@Jamess-MacBook-Pro.local>
2025-05-23 22:54:27 +0800

    添加2025年5月云部署迁移标记

---
+++ migrations/versions/2dd8f9f53975_cloud_deployment_may_2025_marker.py
@@ -0,0 +1,25 @@
+
+"""cloud_deployment_may_2025_marker
+
+Revision ID: 2dd8f9f53975
+Revises: 97cfe4b0eb27
+Create Date: 2025-05-23 22:51:01.996510
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2dd8f9f53975'
+down_revision = '97cfe4b0eb27'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass# 该迁移文件是2025年5月的云端部署标记
